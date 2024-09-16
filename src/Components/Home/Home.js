feb3d339077208c1645a1e73754cc34523538214
hruthvikababburi <130267594+hruthvikababburi@users.noreply.github.com>
2024-09-16 17:41:39 +0530

    Update Home.js
    tried to fetch geolocation api

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -8,8 +8,14 @@ const Home = () => {
   };
   useEffect(() => {
     const API_KEY = "3c935693278ee3ca2e3f1715b605efbb";
-    const url = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit={limit}&appid=${API_KEY}`;
-  }, []);
+    const geolocation_url = `http://api.openweathermap.org/geo/1.0/direct?q=${input}&limit={limit}&appid=${API_KEY}`;
+    fetch(geolocation_url)
+    .then((res)=>{return res.json()})
+    .then((data)=>{console.log})
+    .catch((error)=>{console.error('Errorrrrrr')};alert("Error occured while fetching"))
+    
+
+  }, [input]);
   return (
     <div className="main-cont">
       <h1>Weather Info</h1>
